434395cd6104043bc50a6099daced07b89aa1030
evanream <32232197+evanream@users.noreply.github.com>
2018-05-23 14:12:36 -0700

    Create rollingbloom.cpp

---
+++ src/bench/rollingbloom.cpp
@@ -0,0 +1,31 @@
+// The PerfectionCoin developers
+// file COPYING 
+
+#include <iostream>
+
+#include <bench/bench.h>
+#include <bloom.h>
+
+static void RollingBloom(benchmark::State& state)
+{
+    CRollingBloomFilter filter(120000, 0.000001);
+    std::vector<unsigned char> data(32);
+    uint32_t count = 0;
+    uint64_t match = 0;
+    while (state.KeepRunning()) {
+        count++;
+        data[0] = count;
+        data[1] = count >> 8;
+        data[2] = count >> 16;
+        data[3] = count >> 24;
+        filter.insert(data);
+
+        data[0] = count >> 24;
+        data[1] = count >> 16;
+        data[2] = count >> 8;
+        data[3] = count;
+        match += filter.contains(data);
+    }
+}
+
+BENCHMARK(RollingBloom, 1500 * 1000);
