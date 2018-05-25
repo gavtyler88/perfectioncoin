a2a1ced7e26ad2fab23e00bd19fde84e26dfedf4
evanream <32232197+evanream@users.noreply.github.com>
2018-05-25 14:32:04 -0700

    Create sanity.h

---
+++ src/compat/sanity.h
@@ -0,0 +1,10 @@
+// The PerfectionCoin developers
+// file COPYING 
+
+#ifndef PERFECTIONCOIN_COMPAT_SANITY_H
+#define PERFECTIONCOIN_COMPAT_SANITY_H
+
+bool glibc_sanity_test();
+bool glibcxx_sanity_test();
+
+#endif // PERFECTIONCOIN_COMPAT_SANITY_H
