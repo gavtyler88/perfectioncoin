397f8fb52351401c9052079f32cffe51b8c6d50e
evanream <32232197+evanream@users.noreply.github.com>
2018-05-25 14:33:15 -0700

    Create strnlen.cpp

---
+++ src/compat/strnlen.cpp
@@ -0,0 +1,17 @@
+// The PerfectionCoin developers
+// file COPYING 
+
+#if defined(HAVE_CONFIG_H)
+#include <config/perfectioncoin-config.h>
+#endif
+
+#include <cstring>
+
+#if HAVE_DECL_STRNLEN == 0
+size_t strnlen( const char *start, size_t max_len)
+{
+    const char *end = (const char *)memchr(start, '\0', max_len);
+
+    return end ? (size_t)(end - start) : max_len;
+}
+#endif // HAVE_DECL_STRNLEN
