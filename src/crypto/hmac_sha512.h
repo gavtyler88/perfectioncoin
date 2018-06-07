1a39e538bf5c46a1d3095b5b531a2f1a0f35b855
evanream <32232197+evanream@users.noreply.github.com>
2018-06-06 20:20:38 -0700

    Create hmac_sha512.h

---
+++ src/crypto/hmac_sha512.h
@@ -0,0 +1,31 @@
+// The PerfectionCoin developers
+// file COPYING 
+
+#ifndef PERFECTIONCOIN_CRYPTO_HMAC_SHA512_H
+#define PERFECTIONCOIN_CRYPTO_HMAC_SHA512_H
+
+#include <crypto/sha512.h>
+
+#include <stdint.h>
+#include <stdlib.h>
+
+/** A hasher class for HMAC-SHA-512. */
+class CHMAC_SHA512
+{
+private:
+    CSHA512 outer;
+    CSHA512 inner;
+
+public:
+    static const size_t OUTPUT_SIZE = 64;
+
+    CHMAC_SHA512(const unsigned char* key, size_t keylen);
+    CHMAC_SHA512& Write(const unsigned char* data, size_t len)
+    {
+        inner.Write(data, len);
+        return *this;
+    }
+    void Finalize(unsigned char hash[OUTPUT_SIZE]);
+};
+
+#endif // PERFECTIONCOIN_CRYPTO_HMAC_SHA512_H
