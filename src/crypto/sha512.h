4ec8e2962180564f6f25bd8304a0b4dea3c738dc
evanream <32232197+evanream@users.noreply.github.com>
2018-06-06 20:39:53 -0700

    Create sha512.h

---
+++ src/crypto/sha512.h
@@ -0,0 +1,27 @@
+// The PerfectionCoin developers
+// file COPYING 
+
+#ifndef PERFECTIONCOIN_CRYPTO_SHA512_H
+#define PERFECTIONCOIN_CRYPTO_SHA512_H
+
+#include <stdint.h>
+#include <stdlib.h>
+
+/** A hasher class for SHA-512. */
+class CSHA512
+{
+private:
+    uint64_t s[8];
+    unsigned char buf[128];
+    uint64_t bytes;
+
+public:
+    static const size_t OUTPUT_SIZE = 64;
+
+    CSHA512();
+    CSHA512& Write(const unsigned char* data, size_t len);
+    void Finalize(unsigned char hash[OUTPUT_SIZE]);
+    CSHA512& Reset();
+};
+
+#endif // PERFECTIONCOIN_CRYPTO_SHA512_H
